3b9c42041c6827d6e53500dd3fd72f0290e0c898
johansen.keith@gmail.com <johansen.keith@gmail.com@4d61f70a-55f1-11de-90ee-d5416c60b88e>
2009-06-10 19:06:27 +0000

    Added initial files

---
+++ trunk/LEGION.h
@@ -0,0 +1,93 @@
+#ifndef __LEGION_H__
+#define __LEGION_H__
+
+
+#include <iostream>
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+#include <sstream>
+#include <iostream>
+
+
+
+class ExcitatoryUnit
+{
+ public:
+  double operator()(double x, double y, double input)
+  {
+    return 3.0 * x - (x * x * x) + 2 - y + input;
+  }
+};
+
+class InhibitoryUnit
+{
+ private:
+  static const double beta = .1;
+  static const double eps = .02;
+  static const double gam = 6.0;
+ public:
+  double operator()(double x, double y)
+  {
+    return eps * (gam * (1 + tanh(x / beta)) - y);
+  }
+};
+
+
+struct Node
+{
+    double* weights;
+    double x, y, I;
+
+    Node()
+    {
+        x = 0;
+        y = 0;
+        weights = new double[5];
+        I = 0;
+    }
+};
+
+
+template <class Ex=ExcitatoryUnit, class In=InhibitoryUnit>
+class LEGION
+{
+
+public:
+double z;
+static const int N = 20;
+Node ** legion_nodes;
+void Init_Weights(double, double);
+LEGION();
+void Iteration(double);
+double Max_X_Grid(Node ** a);
+double Min_X_Grid(Node ** a);
+double Rand_Range(double a, double b);
+void Reset(double,double,double,double);
+
+private:
+static const double K = 50.0;
+static const double theta_x = .5;
+static const double theta_zx = .1;
+static const double theta_xz = .1;
+static const double phi = 3.0;
+static const double rho = .02;
+
+Ex ff;
+In gg;
+
+    double S_Inf(double x, double theta);
+    double Gauss_Noise();
+    double Z_Prime(double x, double input);
+    double Sigma_Inf();
+    void Runge_Kutta(double x1, double y1, double &x2, double &y2, double input, double h);
+
+
+};
+
+
+#include "LEGION.impl"
+
+#endif
---
+++ trunk/legion_glut.cpp
@@ -0,0 +1,154 @@
+//Keith Johansen
+//CSE 788.02 Spring 2009
+//LEGION -- Locally Excitatory Globally Inhibitory Neuron, Wang and Terman 1995
+
+#include <iostream>
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include "LEGION.h"
+
+
+
+
+using namespace std;
+
+LEGION<> OHIO_LEGION;
+bool draw = false;
+
+void KeyboardFunc(unsigned char key, int x, int y) {
+    if (key == ' ') {
+        draw = !draw;
+    } else if (key == 27) {
+        exit(1);
+    }
+}
+
+
+
+void processSpecialKeys(int key, int x, int y) {
+
+    switch (key) {
+        case GLUT_KEY_HOME:
+	  OHIO_LEGION.Reset(0,1,0,1);
+    }
+}
+
+
+
+// Initialize OpenGL's rendering modes
+
+void initRendering() {
+    glEnable(GL_DEPTH_TEST);
+
+    glPointSize(8);
+    glLineWidth(5);
+
+    glEnable(GL_POINT_SMOOTH);
+    glEnable(GL_LINE_SMOOTH);
+    glHint(GL_POINT_SMOOTH_HINT, GL_NICEST);
+    glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+void drawLEGION() {
+    double max = OHIO_LEGION.Max_X_Grid(OHIO_LEGION.legion_nodes);
+    double min = OHIO_LEGION.Min_X_Grid(OHIO_LEGION.legion_nodes);
+
+    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    float t = 0.1f;
+    glLoadIdentity();
+    float posx;
+    float posy;
+
+
+    double diff = 0.0f, col = 0.0f;
+    double sep = 1.0 / (float) OHIO_LEGION.N;
+
+    glBegin(GL_QUADS);
+    for (int i = 1; i <= OHIO_LEGION.N; i++) {
+        posx = -1 + sep * i * 1.85;
+        for (int j = 1; j <= OHIO_LEGION.N; j++) {
+            col = (OHIO_LEGION.legion_nodes[i][j].x - min) / (max - min);
+            diff = col * .02 + .01;
+            if (OHIO_LEGION.legion_nodes[i][j].I > 0) {
+                cout << col << " ";
+            }
+            posy = 1 - sep * j * 1.85;
+            glColor3f(0, col, 0);
+            glVertex3f(posx - diff, posy - diff, 0.0f);
+            glVertex3f(posx + diff, posy - diff, 0.0f);
+            glVertex3f(posx + diff, posy + diff, 0.0f);
+            glVertex3f(posx - diff, posy + diff, 0.0f);
+        }
+    }
+    cout << OHIO_LEGION.z;
+    cout << endl;
+
+    glEnd();
+    glutSwapBuffers();
+}
+
+void loop() {
+    if (draw == true) {
+        //    usleep(10000);
+        OHIO_LEGION.Iteration(.2);
+        glutPostRedisplay();
+        glutSwapBuffers();
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+
+
+    srand(time(NULL));
+
+    string s;
+    if (getline(cin, s) != NULL) {
+        for (int i = 1; i <= OHIO_LEGION.N; i++) {
+            for (int j = 1; j <= OHIO_LEGION.N; j++) {
+                OHIO_LEGION.legion_nodes[i][j].I = (s[(j - 1) * OHIO_LEGION.N + i - 1] - '0')*.22 - .02;
+                OHIO_LEGION.legion_nodes[i][j].x = OHIO_LEGION.Rand_Range(0, 1);
+                OHIO_LEGION.legion_nodes[i][j].y = OHIO_LEGION.Rand_Range(0, 1);
+            }
+        }
+
+    }
+
+    OHIO_LEGION.Init_Weights(6.0, 1.5);
+
+    glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+
+    glutInitWindowPosition(20, 60);
+    glutInitWindowSize(720, 720);
+    glutCreateWindow("");
+
+    initRendering();
+    glutKeyboardFunc(KeyboardFunc);
+    glutSpecialFunc(processSpecialKeys);
+
+    glutDisplayFunc(drawLEGION);
+    glutIdleFunc(loop);
+
+    glutMainLoop();
+
+    return (0);
+
+}
+
+
+
+
+
